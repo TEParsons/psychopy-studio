963bd16e71787e88565af130353464ab0c373edc
TEParsons <todd.e.parsons@gmail.com>
2026-03-12 15:27:09 +0000

    NF: Create a persistent app data file which stores profiles and will store open frames

---
+++ src/lib/appdata.svelte.js
@@ -0,0 +1,70 @@
+import FallbackComponentProfiles from "$lib/experiment/fallbacks/components.json";
+import FallbackLoopProfiles from "$lib/experiment/fallbacks/loops.json";
+import FallbackDeviceProfiles from "$lib/experiment/fallbacks/devices.json";
+import PreferencesProfile from "$lib/preferences.json"
+import path from "path-browserify"
+
+
+class AppData {
+    profiles = $state({
+        components: FallbackComponentProfiles,
+        loops: FallbackLoopProfiles,
+        devices: FallbackDeviceProfiles,
+        preferences: PreferencesProfile
+    })
+
+    frames = $state({
+        builder: [],
+        coder: [],
+        runner: []
+    })
+
+    constructor() {
+        this.ready = this.load()
+    }
+
+    async load() {
+        // do nothing if no electron
+        if ( !electron ) {
+            return
+        }
+        // get file path
+        let file = path.join(
+            await electron.paths.user(),
+            "appdata.json"
+        )
+        // do nothing if file doesn't exist yet
+        if (!(await electron.files.exists(file))) {
+            return
+        }
+        // load from file
+        let data = JSON.parse(
+            await electron.files.load(file)
+        )
+        // apply
+        Object.assign(this.profiles, data.profiles)
+        Object.assign(this.frames, data.frames)
+    }
+
+    async save() {
+        // do nothing if no electron
+        if ( !electron ) {
+            return
+        }
+        // get file path
+        let file = path.join(
+            await electron.paths.user(),
+            "appdata.json"
+        )
+        // write
+        await electron.files.save(
+            file,
+            JSON.stringify({
+                profiles: this.profiles,
+                files: this.frames
+            }, undefined, 4)
+        )
+    }
+}
+
+export var appdata = new AppData()
--- src/lib/experiment/profiles.svelte.js
+++ src/lib/experiment/profiles.svelte.js
@@ -1,16 +1,9 @@
-import FallbackComponentProfiles from "$lib/experiment/fallbacks/components.json";
-import FallbackLoopProfiles from "$lib/experiment/fallbacks/loops.json";
-import FallbackDeviceProfiles from "$lib/experiment/fallbacks/devices.json";
-import FallbackPreferencesProfile from "$lib/preferences.json";
 import { python } from "$lib/globals.svelte";
+import { appdata } from "$lib/appdata.svelte";
 
 
-export var profiles = $state({
-    components: FallbackComponentProfiles,
-    loops: FallbackLoopProfiles,
-    devices: FallbackDeviceProfiles,
-    preferences: FallbackPreferencesProfile
-})
+export var profiles = $state($state.snapshot(appdata.profiles))
+appdata.ready.then(() => Object.assign(profiles, appdata.profiles))
 
 export var pending = $state({
     components: Promise.withResolvers().promise,
@@ -21,36 +14,43 @@ export var pending = $state({
 
 // populate on Liaison starting (if it ever does)
 if ( python ) {
-    python.liaison.ready("app").then(
-        () => {
-            // get components
-            pending.components = python.liaison.send("app", {
-                command: "run",
-                args: [
-                    "psychopy.experiment:getElementProfiles"
-                ]
-            }).then(
-                data => Object.assign(profiles.components, data)
-            )
-            // get loops
-            pending.loops = python.liaison.send("app", {
-                command: "run",
-                args: [
-                    "psychopy.experiment:getLoopProfiles"
-                ]
-            }).then(
-                data => Object.assign(profiles.loops, data)
-            )
-            // get devices
-            pending.devices = python.liaison.send("app", {
-                command: "run",
-                args: [
-                    "psychopy.experiment:getDeviceProfiles"
-                ]
-            }).then(
-                resp => Object.assign(profiles.devices, resp)
-            )
-            // todo: get prefs
-        }
-    )
+    python.liaison.ready("app").then(() => {
+        // get components
+        pending.components = python.liaison.send("app", {
+            command: "run",
+            args: [
+                "psychopy.experiment:getElementProfiles"
+            ]
+        }).then(
+            data => Object.assign(profiles.components, data)
+        )
+        // get loops
+        pending.loops = python.liaison.send("app", {
+            command: "run",
+            args: [
+                "psychopy.experiment:getLoopProfiles"
+            ]
+        }).then(
+            data => Object.assign(profiles.loops, data)
+        )
+        // get devices
+        pending.devices = python.liaison.send("app", {
+            command: "run",
+            args: [
+                "psychopy.experiment:getDeviceProfiles"
+            ]
+        }).then(
+            resp => Object.assign(profiles.devices, resp)
+        )
+        // save profiles if possible
+        Promise.all([
+            pending.components,
+            pending.loops,
+            pending.devices
+        ]).then(() => {
+            Object.assign(appdata.profiles, profiles)
+            appdata.save()
+        })
+    })
 }
+
